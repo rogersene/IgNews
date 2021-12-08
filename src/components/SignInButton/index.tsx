b37e1706dd5cc238edc8ec4761b90ca140a654c9
rogersene <rogersenefaria@gmail.com>
2021-12-08 15:46:59 -0300

    Update project Ignews

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+export function SignInButton() {
+    const [session] = useSession()
+
+
+    console.log(session);
+
+    return session ? (
+        <button type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}    >
+            <FaGithub color="#04D361" />
+            {session.user.name}
+        <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+
+    ) : (
+        <button type="button"
+                className={styles.signInButton}
+                onClick={() => signIn('github')}    >
+            <FaGithub color="#EBA417" />
+            Sign in with Github
+        </button>
+    );
+}
---
+++ src/services/fauna.ts
@@ -0,0 +1,11 @@
+import { Client } from 'faunadb'
+
+
+export const fauna = new Client({
+    secret: process.env.FAUNADB_KEY
+})
+
+
+
+
+
